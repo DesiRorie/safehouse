797274dce625905a3da8dedb8c3333e6aaa4e8b0
DesiRorie <desirorie@icloud.com>
2023-04-22 15:13:48 -0400

    "needed new features"

--- src/components/AccountBox.jsx
+++ src/components/AccountBox.jsx
@@ -3,22 +3,27 @@ import { Link } from "react-router-dom";
 
 const AccountBox = () => {
   return (
-    <Link to="/accounts" style={{ textDecoration: "none", color: "inherit" }}>
+    <>
       <div className="accountBoxContainer">
-        <div className="accountBoxes">
-          <h4>Bank Accounts (1)</h4>
-          <div className="accountInfo">
-            <p>Checkings(...1912 ) &gt; </p>
-            <span style={{ fontWeight: "bold", marginBottom: "10px" }}>
-              $1,000
-            </span>
-            <span style={{ fontWeight: "lighter", color: "#888" }}>
-              <i>Available balance</i>
-            </span>
+        <Link
+          to="/accounts"
+          style={{ textDecoration: "none", color: "inherit" }}
+        >
+          <div className="accountBoxes">
+            <h4>Bank Accounts (1)</h4>
+            <div className="accountInfo">
+              <p>Checkings(...1912 ) &gt; </p>
+              <span style={{ fontWeight: "bold", marginBottom: "10px" }}>
+                $1,000
+              </span>
+              <span style={{ fontWeight: "lighter", color: "#888" }}>
+                <i>Available balance</i>
+              </span>
+            </div>
           </div>
-        </div>
+        </Link>
       </div>
-    </Link>
+    </>
   );
 };
 
--- src/components/AccountPage.jsx
+++ src/components/AccountPage.jsx
@@ -4,7 +4,23 @@ const AccountPage = () => {
   return (
     <div className="accountPage">
       <h1>Total Checking(...1912)</h1>
-      <div className="checkingDiv">s</div>
+      <div className="checkingDiv">
+        <div className="checkingDivInfo">
+          <span>$1,000</span>
+          <span id="availableBalance">Available balance</span>
+        </div>
+        <h3>Account details</h3>
+        <div>
+          <div className="balanceDiv">
+            <span id="availableBalance1">Available balance</span>
+            <span id="availableBalance2">$1,000</span>
+          </div>
+          <div className="balanceDiv">
+            <span id="availableBalance1">Present balance</span>
+            <span id="availableBalance2">$1,000</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
